9e8d16a5f70a428bb822fd33fb89dc45fad03069
WtrpXcal <DECLANMEURIG@GMAIL.COM>
2023-10-18 16:01:09 -0400

    Added Ability to view others notes
    Fixed noteid resetting when giving notes to another player
    Added private note option incase you don't want others interacting with a persons notebook - might add cop check or break lock feature later-

--- config.lua
+++ config.lua
@@ -1,5 +1,7 @@
 Config = {}
 
+Config.PrivateNotes = true -- If set to true, only the player who created the note can see it. If set to false, everyone can see it.
+
 function ToggleAnimation(bool)
     if bool then
         exports.scully_emotemenu:PlayByCommand('notepad') -- You'll have to change to whatever emote resource you use. 
--- sv_notes.lua
+++ sv_notes.lua
@@ -1,5 +1,7 @@
 local ox_inv = exports.ox_inventory
 
+local hasgiven
+
 local function GenerateNotepadId()
     local characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     local gen = ""
@@ -17,6 +19,10 @@ QBCore.Functions.CreateUseableItem("notepad", function(source, item)
     local Player = QBCore.Functions.GetPlayer(src)
     if ox_inv:Search(src, 'count', item.name) > 0 then
         if item.metadata.noteid then
+            if Config.PrivateNotes and item.metadata.noteowner ~= Player.PlayerData.citizenid then
+                QBCore.Functions.Notify(src, 'You do not own this notepad.', 'error')
+                return
+            end
             TriggerClientEvent("randol_notes:client:useItem", src, item.metadata.noteid)
         end
     end
@@ -26,7 +32,12 @@ lib.callback.register('randol_notes:server:getNotes', function(source, noteid)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local notesData = {}
-    local result = MySQL.query.await('SELECT notes FROM rnotes WHERE citizenid = ? and noteid = ?', {Player.PlayerData.citizenid, noteid})
+    local result 
+    if Config.PrivateNotes then
+        result = MySQL.query.await('SELECT notes FROM rnotes WHERE citizenid = ? and noteid = ?', {Player.PlayerData.citizenid, noteid})
+    else
+        result = MySQL.query.await('SELECT notes FROM rnotes WHERE noteid = ?', {noteid})
+    end
 
     if result and result[1] then
         notesData = json.decode(result[1].notes)
@@ -42,7 +53,12 @@ RegisterNetEvent('randol_notes:server:newNote', function(note, isSigned, noteid)
     local Player = QBCore.Functions.GetPlayer(src)
     local currentDate = os.date("%d-%m-%Y")
     local existingNotes = {}
-    local result = MySQL.query.await('SELECT notes FROM rnotes WHERE citizenid = ? and noteid = ?', {Player.PlayerData.citizenid, noteid})
+    local result 
+    if Config.PrivateNotes then
+        result = MySQL.query.await('SELECT notes FROM rnotes WHERE citizenid = ? and noteid = ?', {Player.PlayerData.citizenid, noteid})
+    else
+        result = MySQL.query.await('SELECT notes FROM rnotes WHERE noteid = ?', {noteid})
+    end
 
     if result and result[1] then
         existingNotes = json.decode(result[1].notes)
@@ -61,7 +77,12 @@ RegisterNetEvent('randol_notes:server:deleteNote', function(data, noteid)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local noteId = tonumber(data.id)
-    local result = MySQL.query.await('SELECT notes FROM rnotes WHERE citizenid = ? and noteid = ?', {Player.PlayerData.citizenid, noteid})
+    local result 
+    if Config.PrivateNotes then
+        result = MySQL.query.await('SELECT notes FROM rnotes WHERE citizenid = ? and noteid = ?', {Player.PlayerData.citizenid, noteid})
+    else
+        result = MySQL.query.await('SELECT notes FROM rnotes WHERE noteid = ?', {noteid})
+    end
     local existingNotes = {}
     if result[1] then
         local removed = false
@@ -103,8 +124,12 @@ RegisterNetEvent('randol_notes:server:ripNote', function(data, noteid)
     local date = data.date
     local description = ('%s | %s\n\nNote: %s'):format(date, name, message)
     local metadata = { note = message, description = description, }
-
-    local result = MySQL.query.await('SELECT notes FROM rnotes WHERE citizenid = ? and noteid = ?', {Player.PlayerData.citizenid, noteid})
+    local result 
+    if Config.PrivateNotes then
+        result = MySQL.query.await('SELECT notes FROM rnotes WHERE citizenid = ? and noteid = ?', {Player.PlayerData.citizenid, noteid})
+    else
+        result = MySQL.query.await('SELECT notes FROM rnotes WHERE noteid = ?', { noteid})
+    end
     local existingNotes = {}
 
     if result[1] then
@@ -126,12 +151,28 @@ RegisterNetEvent('randol_notes:server:ripNote', function(data, noteid)
     end
 end)
 
+local NOTEPAD_GIVE = ox_inv:registerHook('swapItems', function(payload)
+    hasgiven = true
+    CreateThread(function()
+        Wait(100)
+        hasgiven = false
+    end)
+end, { print = false, itemFilter = { notepad = true } })
+
 local NOTEPAD_HOOK = ox_inv:registerHook('createItem', function(payload)
     local metadata = payload.metadata
     local Player = QBCore.Functions.GetPlayer(payload.inventoryId)
     local charname = Player.PlayerData.charinfo.firstname..' '..Player.PlayerData.charinfo.lastname
-    metadata.noteid = GenerateNotepadId()
-    metadata.description = ('Barcode: %s\n\nOwner: %s'):format(metadata.noteid, charname)
-    MySQL.insert.await('INSERT INTO rnotes (citizenid, notes, noteid) VALUES (?, ?, ?)', {Player.PlayerData.citizenid, json.encode({}), metadata.noteid})
-    return metadata
+    if not hasgiven then
+        metadata.noteid = GenerateNotepadId()
+        metadata.noteowner = Player.PlayerData.citizenid
+        metadata.description = ('Barcode: %s\n\nOwner: %s'):format(metadata.noteid, charname)
+        MySQL.insert.await('INSERT INTO rnotes (citizenid, notes, noteid) VALUES (?, ?, ?)', {Player.PlayerData.citizenid, json.encode({}), metadata.noteid})
+        return metadata
+    else
+        hasgiven = false
+        return metadata
+    end
+
 end, { print = false, itemFilter = { notepad = true } })
+
